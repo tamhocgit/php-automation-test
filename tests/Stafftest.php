481ee4c4308a560f5964bd5e02711aa067dcbd21
tam <tantran@gmail.com>
2021-08-04 15:01:07 +0700

    2020

--- tests/Stafftest.php
+++ tests/Stafftest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "Tran";
+        $staff->first_name = "Nguyen";
         $this->assertEquals('Tran', $staff->getFullName());
     }
 }
