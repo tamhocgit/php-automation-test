12e7f4264ceee9b797277795a5e9c10c48a4a2a1
root <root@ip-172-31-4-184.ap-southeast-1.compute.internal>
2021-08-04 07:04:36 +0000

    Add file to git

---
+++ tests/Stafftest.php
@@ -0,0 +1,33 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class StaffTest extends TestCase
+{
+    public function testReturnsFullName()
+    {
+        require('Staff.php');
+
+        $staff = new Staff;
+
+        $staff->first_name = "Tran";
+        $staff->last_name = "Tam";
+
+        $this->assertEquals('Tran Tam', $staff->getFullName());
+    }
+    public function testFullNameIsEmptyByDefault()
+    {
+        $staff = new Staff;
+        $this->assertEquals('', $staff->getFullName());
+    }
+
+    /**
+     * @test
+     */
+    public function only_first_name()
+    {
+        $staff = new Staff;
+        $staff->first_name = "Tran";
+        $this->assertEquals('Tran', $staff->getFullName());
+    }
+}
